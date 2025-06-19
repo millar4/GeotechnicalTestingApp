1532e73d230d296d6a3465e830b5622096af0c62
Millar Scandrett <ju22534@bristol.ac.uk>
2025-06-19 11:50:36 +0100

    Concrete starting to be added

--- backend/database/database.sql
+++ backend/database/database.sql
@@ -58,6 +58,34 @@ CREATE TABLE IF NOT EXISTS RocksTable(
 );
 
 -- ============================================
+-- 2) Create ConcreteTable
+-- ============================================
+CREATE TABLE IF NOT EXISTS ConcreteTable(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    myGroup VARCHAR(100),
+    test VARCHAR(1000),
+    symbol VARCHAR(10),
+    parameters VARCHAR(1000),
+    testMethod VARCHAR(100),
+    alt1 VARCHAR(100),
+    alt2 VARCHAR(100),
+    alt3 VARCHAR(100),
+    sampleType VARCHAR(30),
+    fieldSampleMass VARCHAR(100),
+    specimenType VARCHAR(1000),
+    specimenMass VARCHAR(100),
+    specimenNumbers VARCHAR(100),
+    specimenD VARCHAR(100),
+    specimenL VARCHAR(100),
+    specimenW VARCHAR(100),
+    specimenH VARCHAR(100),
+    specimenMaxGrainSize VARCHAR(100),
+    specimenMaxGrainFraction VARCHAR(100),
+    schedulingNotes TEXT(300)
+);
+
+
+-- ============================================
 -- 3) Create AggregateTable
 -- ============================================
 CREATE TABLE IF NOT EXISTS AggregateTable (
@@ -131,6 +159,24 @@ IGNORE 1 LINES
     specimenD, specimenL, specimenW, specimenH,
     specimenMaxGrainSize, specimenMaxGrainFraction
 );
+
+-- ============================================
+-- 5) Load CSV data into ConcreteTable
+-- ============================================
+LOAD DATA INFILE '/var/lib/mysql-files/concrete.csv'
+INTO TABLE ConcreteTable
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(
+    myGroup, test, symbol, parameters, testMethod,
+    alt1, alt2, alt3, sampleType, fieldSampleMass,
+    specimenType, specimenMass, specimenNumbers,
+    specimenD, specimenL, specimenW, specimenH,
+    specimenMaxGrainSize, specimenMaxGrainFraction
+);
+
 
 
 CREATE TABLE IF NOT EXISTS users (
