32147c610be1cca44fcae7170d2cd8c89c07232e
Millar Scandrett <ju22534@bristol.ac.uk>
2025-06-26 10:41:42 +0100

    update

--- backend/database/database.sql
+++ backend/database/database.sql
@@ -31,6 +31,31 @@ CREATE TABLE IF NOT EXISTS GeotechnicalTable (
     specimenMaxGrainFraction VARCHAR(100)
 );
 
+
+CREATE TABLE IF NOT EXISTS InSituUser (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    myGroup VARCHAR(100),
+    classification VARCHAR(100),
+    test VARCHAR(1000),
+    symbol VARCHAR(10),
+    parameters VARCHAR(1000),
+    testMethod VARCHAR(100),
+    alt1 VARCHAR(100),
+    alt2 VARCHAR(100),
+    alt3 VARCHAR(100),
+    sampleType VARCHAR(30),
+    fieldSampleMass VARCHAR(100),
+    specimenType VARCHAR(1000),
+    specimenMass VARCHAR(100),
+    specimenNumbers VARCHAR(100),
+    specimenD VARCHAR(100),
+    specimenL VARCHAR(100),
+    specimenW VARCHAR(100),
+    specimenH VARCHAR(100),
+    specimenMaxGrainSize VARCHAR(100),
+    specimenMaxGrainFraction VARCHAR(100)
+);
+
 -- ============================================
 -- 2) Create RocksTable
 -- ============================================
